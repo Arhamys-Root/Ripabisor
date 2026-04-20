e3cb49f419bd9cc147e755ebff03d5fa6c90b59d
Arhamys Root <arhamys14@gmail.com>
2026-04-20 12:09:42 +0100

    Aniadimos atributos

--- src/net/salesianos/restaurante/Restaurante.java
+++ src/net/salesianos/restaurante/Restaurante.java
@@ -1,5 +1,8 @@
 package net.salesianos.restaurante;
 
 public class Restaurante {
-    
+    private String nombre;
+    private String ciudad;
+    private String tipoCocina;
+    private int puntuacion;
 }
