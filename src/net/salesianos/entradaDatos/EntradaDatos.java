6789a51dc781f358af872dcb41fa7bf4f4d07312
Arhamys Root <arhamys14@gmail.com>
2026-04-20 12:43:59 +0100

    Crear metodo pedir texto

--- src/net/salesianos/entradaDatos/EntradaDatos.java
+++ src/net/salesianos/entradaDatos/EntradaDatos.java
@@ -2,3 +2,18 @@ package net.salesianos.entradaDatos;
 
 import javax.swing.JOptionPane;
 
+public class EntradaDatos {
+
+    public static String pedirTexto(String mensaje) {
+        String texto;
+        do {
+            texto = JOptionPane.showInputDialog(null, mensaje);
+            if (texto == null) {
+                JOptionPane.showMessageDialog(null, "No puedes cancelar aquí.");
+            } else {
+                texto = texto.trim();
+            }
+        } while (texto == null || texto.isEmpty());
+        return texto;
+    }
+}
