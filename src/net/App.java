a05caa504c6837a42732288b298c33f5ca6afdbc
Arhamys Root <arhamys14@gmail.com>
2026-04-20 13:01:03 +0100

    aniamos el metodo aniadir restaurante

--- src/net/App.java
+++ src/net/App.java
@@ -1,5 +1,7 @@
 package net;
 
+import javax.swing.JOptionPane;
+
 import net.salesianos.entradaDatos.EntradaDatos;
 import net.salesianos.lista.Lista;
 import net.salesianos.restaurante.Restaurante;
@@ -7,5 +9,17 @@ import net.salesianos.restaurante.Restaurante;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public static void anadirRestaurante(Lista service) {
+        String nombre = EntradaDatos.pedirTexto("Introduce el nombre del restaurante:");
+        String ciudad = EntradaDatos.pedirTexto("Introduce la ciudad:");
+        String tipoCocina = EntradaDatos.pedirTexto("Introduce el tipo de cocina:");
+        int puntuacion = EntradaDatos.pedirPuntuacion("Introduce la puntuación (1-5):");
+
+        Restaurante restaurante = new Restaurante(nombre, ciudad, tipoCocina, puntuacion);
+        service.anadir(restaurante);
+
+        JOptionPane.showMessageDialog(null, "Restaurante añadido correctamente.");
     }
 }
