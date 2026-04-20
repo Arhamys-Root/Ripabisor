0e234a67c3d1f271061da52b1087c4f4708c99c1
Arhamys Root <arhamys14@gmail.com>
2026-04-20 12:26:26 +0100

    Creamos array list lista

--- src/net/salesianos/lista/Lista.java
+++ src/net/salesianos/lista/Lista.java
@@ -1,5 +1,10 @@
 package net.salesianos.lista;
 
+import net.salesianos.restaurante.Restaurante;
+import java.util.ArrayList;
+
 public class Lista {
+    private ArrayList<Restaurante> lista;
+    
     
 }
