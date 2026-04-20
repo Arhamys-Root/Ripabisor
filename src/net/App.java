8604fd68288baed43de479765f7276b7cc978831
Arhamys Root <arhamys14@gmail.com>
2026-04-20 12:59:20 +0100

    Crear imports

--- src/net/App.java
+++ src/net/App.java
@@ -1,3 +1,9 @@
+package net;
+
+import net.salesianos.entradaDatos.EntradaDatos;
+import net.salesianos.lista.Lista;
+import net.salesianos.restaurante.Restaurante;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
