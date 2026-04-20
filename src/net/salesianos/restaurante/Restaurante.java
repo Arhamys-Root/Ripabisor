ae69997c5e2493dd2237436b6250dc3585e0c10a
Arhamys Root <arhamys14@gmail.com>
2026-04-20 12:10:13 +0100

    Aniadimos constructor

--- src/net/salesianos/restaurante/Restaurante.java
+++ src/net/salesianos/restaurante/Restaurante.java
@@ -5,4 +5,11 @@ public class Restaurante {
     private String ciudad;
     private String tipoCocina;
     private int puntuacion;
+
+    public Restaurante(String nombre, String ciudad, String tipoCocina, int puntuacion) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.tipoCocina = tipoCocina;
+        this.puntuacion = puntuacion;
+    }
 }
